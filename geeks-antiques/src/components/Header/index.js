66b46a787736a80bb1833fc2146355b88878aa61
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-21 14:32:06 -0700

    created title for page and styled page

---
+++ geeks-antiques/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div>
+      <header className='page-header'>The Antique Geek</header>
+    </div>
+  );
+}
+export default Header;
