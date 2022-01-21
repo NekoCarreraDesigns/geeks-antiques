67a84dd6e77c3a365c251098dfb5fd22d5282e1f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-21 14:50:16 -0700

    added navbar to header, styled navbar

--- geeks-antiques/src/components/Header/index.js
+++ geeks-antiques/src/components/Header/index.js
@@ -4,6 +4,20 @@ import "./style.css";
 function Header() {
   return (
     <div>
+      <nav className='page-navbar'>
+        <a classname='page-navbar-link' href='/coins'>
+          Numisma
+        </a>
+        {"   "}
+        <a classname='page-navbar-link' href='/stamps'>
+          Philately
+        </a>
+        {"   "}
+        <a classname='page-navbar-link' href='/antiques'>
+          Antiques
+        </a>
+        {"   "}
+      </nav>
       <header className='page-header'>The Antique Geek</header>
     </div>
   );
