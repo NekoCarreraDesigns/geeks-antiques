f7bd958a2eac980b84c37f86785c18287a00956f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-24 13:17:26 -0700

    added router npm, and created routes, as well created the philately page

--- geeks-antiques/src/App.js
+++ geeks-antiques/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Philately from "./components/Philately";
 import PageBody from "./components/Page-Body";
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <PageBody />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<PageBody />}></Route>
+          <Route path='/stamps' element={<Philately />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ geeks-antiques/src/components/Philately/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Philatley() {
+  return (
+    <div>
+      <header className='philately-header'>Philately</header>
+    </div>
+  );
+}
+
+export default Philatley;
