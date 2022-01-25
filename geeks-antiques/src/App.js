aaf971283587e8cbd05e6ba86a603255e207d81b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-25 14:43:00 -0700

    created and styled stamp view page

--- geeks-antiques/src/App.js
+++ geeks-antiques/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Philately from "./components/Philately";
 import PageBody from "./components/Page-Body";
+import StampsView from "./components/Stamps-View";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<PageBody />}></Route>
           <Route path='/stamps' element={<Philately />}></Route>
+          <Route path='stamps-view' element={<StampsView />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ geeks-antiques/src/components/Stamps-View/index.js
@@ -0,0 +1,118 @@
+import React from "react";
+import "./style.css";
+
+function StampsView() {
+  return (
+    <div>
+      <div className='stamp-row'>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1846 Russian Coat of Arms(red) 2k <br /> MNHOGXF/SU $1,000
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1846 Russian Coat of Arms(green) <br /> MNHOGXF/SU $35,000
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+      <div className='stamp-row-1'>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+      <div className='stamp-row-2'>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+
+        <div className='stamp-column'>
+          <img alt='stamp' src='http://placehold.jp/150x150.png' />
+          <p className='description'>1846 MNHOGXF/SU $1,000</p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StampsView;
