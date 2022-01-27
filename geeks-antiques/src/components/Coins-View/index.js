8d2b2785377d4e59d0ac8997e4fd2cb87c42b9e8
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-27 14:56:39 -0700

    created coin view page, and styled page

---
+++ geeks-antiques/src/components/Coins-View/index.js
@@ -0,0 +1,139 @@
+import React from "react";
+import "./style.css";
+
+function CoinsView() {
+  return (
+    <div>
+      <div className='coin-row'>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1892cc Morgan Silver Dollar
+            <br /> VF-20 $185
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1904-O Morgan Silver Dollar
+            <br /> MS-66 $400
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1999 Susan B. Anthony Dollar
+            <br /> PF-70 $30
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+      <div className='coin-row-1'>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+      <div className='coin-row-2'>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+        <div className='coin-column'>
+          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <p className='description'>
+            1919 Mercury Dime <br /> VF-20 $1.75
+          </p>
+          <br />
+          <a className='purchase-link' href='/purchase-message'>
+            Purchase
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CoinsView;
