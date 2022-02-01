beaa6ace19a01066e0f68425f262325defae679a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-01 14:26:05 -0700

    Added picture to coin view page

--- geeks-antiques/src/components/Coins-View/index.js
+++ geeks-antiques/src/components/Coins-View/index.js
@@ -6,9 +6,10 @@ function CoinsView() {
     <div>
       <div className='coin-row'>
         <div className='coin-column'>
-          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <img alt='coin' src='./assets/Mercury_1942_Obverse.jpg' />
+          <img alt='coin' src='./assets/Mercury_1942_Reverse.jpg' />
           <p className='description'>
-            1919 Mercury Dime <br /> VF-20 $1.75
+            1942 Mercury Dime <br /> VF-20 $1.75
           </p>
           <br />
           <a className='purchase-link' href='/purchase-message'>
@@ -16,7 +17,8 @@ function CoinsView() {
           </a>
         </div>
         <div className='coin-column'>
-          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <img alt='coin' src='./assets/Morgan_1892_Obverse.jpg' />
+          <img alt='coin' src='./assets/Morgan_1892_Reverse.jpg' />
           <p className='description'>
             1892cc Morgan Silver Dollar
             <br /> VF-20 $185
@@ -27,7 +29,8 @@ function CoinsView() {
           </a>
         </div>
         <div className='coin-column'>
-          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <img alt='coin' src='./assets/Morgan_1904_Obverse.jpg' />
+          <img alt='coin' src='./assets/Morgan_1904_Reverse.jpg' />
           <p className='description'>
             1904-O Morgan Silver Dollar
             <br /> MS-66 $400
@@ -38,10 +41,10 @@ function CoinsView() {
           </a>
         </div>
         <div className='coin-column'>
-          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <img alt='coin' src='./assets/Susan_B_Anthony.jpg' />
           <p className='description'>
             1999 Susan B. Anthony Dollar
-            <br /> PF-70 $30
+            <br /> PF-70 $160
           </p>
           <br />
           <a className='purchase-link' href='/purchase-message'>
@@ -51,9 +54,10 @@ function CoinsView() {
       </div>
       <div className='coin-row-1'>
         <div className='coin-column'>
-          <img alt='coin' src='http://placehold.jp/150x150.png' />
+          <img alt='coin' src='./assets/Buffalo_1936_Obverse.jpg' />
+          <img alt='coin' src='./assets/Buffalo_1936_Reverse.jpg' />
           <p className='description'>
-            1919 Mercury Dime <br /> VF-20 $1.75
+            1936 Buffalo Nickel <br /> F-12 $2.00
           </p>
           <br />
           <a className='purchase-link' href='/purchase-message'>
