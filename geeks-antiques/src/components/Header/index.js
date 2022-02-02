82f0236521474def6b0f75d1eb36798aeb54c138
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-02 13:55:29 -0700

    added links to views pages in navbar

--- geeks-antiques/src/components/Header/index.js
+++ geeks-antiques/src/components/Header/index.js
@@ -17,6 +17,18 @@ function Header() {
           Antiques
         </a>
         {"   "}
+        <br />
+        <a className='page-navbar-link' href='/coins-view'>
+          Coins
+        </a>
+        {"  "}
+        <a className='page-navbar-link' href='/stamps-view'>
+          Stamps
+        </a>
+        {"  "}
+        <a className='page-navbar-link' href='/antiques-view'>
+          Pieces
+        </a>
       </nav>
       <header className='page-header'>The Antique Geek</header>
     </div>
